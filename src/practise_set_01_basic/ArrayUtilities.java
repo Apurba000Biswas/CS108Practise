714a316139f7d4599e85aeac1ae86b6e3da415c6
apurba <apurba10024@gmail.com>
2019-04-18 03:52:13 +0600

    1.2 Added Comparison between Two Arrays

--- src/practise_set_01_basic/ArrayUtilities.java
+++ src/practise_set_01_basic/ArrayUtilities.java
@@ -15,10 +15,12 @@ public class ArrayUtilities {
 		System.out.print("Array : ");
 		printArray(array);
 		
+		// Array Sorting
 		System.out.print("Sorted Array : ");
 		Arrays.sort(array);
 		printArray(array);
 		
+		// Array Coping
 		//System.arraycopy(source-array, source-index, dest-array, dest-index, length-to-copy);
 		int[] copyArray = new int[10]; 
 		System.arraycopy(array, 4, copyArray, 4, 3);
@@ -26,8 +28,16 @@ public class ArrayUtilities {
 		printArray(copyArray);
 		
 		System.arraycopy(copyArray, 4, copyArray, 0, 2);
-		System.out.print("Copy Array : ");
+		System.out.print("Again Copy Array : ");
 		printArray(copyArray);
+		
+		// Array Comparison
+		System.arraycopy(array, 0, copyArray, 0, array.length);
+		System.out.print("New Copy : ");
+		printArray(copyArray);
+		System.out.println("Is Equal?:  " + Arrays.equals(array, copyArray));
+		// Note : To compare multidimensional Array Use Arrays.equals(a, b)
+ 		
 	}
 	
 	private void printArray(int[] array) {
